3a52608f2f504cd69f96e07c0b7036e998559bf8
Rafal <rslabon@gmail.com>
2012-01-10 21:33:27 +0100

    expression language example

--- src/main/java/home/ElServlet.java
+++ src/main/java/home/ElServlet.java
@@ -6,6 +6,10 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class ElServlet extends HttpServlet {
 
@@ -21,9 +25,17 @@ public class ElServlet extends HttpServlet {
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp)
 			throws ServletException, IOException {
 		resp.setContentType("text/html;charset=UTF-8");
+		
+		HttpSession httpSession = req.getSession();
+		
+		Logger logger = LoggerFactory.getLogger(this.getClass());
+		
+		logger.info("ElServlet session is: " + httpSession);
+		logger.info(resp.encodeURL("/AddNumber.do"));
+		
 		int result = 5 + Integer.parseInt(req.getParameter("number"));
 		req.setAttribute("result", result);
-		req.getRequestDispatcher("/result.jsp").forward(req, resp);
+		req.getRequestDispatcher(resp.encodeURL("/Result.jsp")).include(req, resp);
 	}
 
 }
